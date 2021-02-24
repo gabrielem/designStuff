47ed50816ec311132340839860b9e987a3ce2af5
gabrielem <gabriele.marazzi@gmail.com>
2021-02-24 14:38:09 +0100

    Update Readme and add travis

--- README.md
+++ README.md
@@ -5,3 +5,13 @@ A collection of react component for design purpose. Buttons, menu ecc.
 # Installation
 
 `npm i designStuff`
+
+
+
+### Reference on the process of this NPM package build
+
+- git init
+- npm init
+- npx sb init
+- npm t - test with jest
+- npm publish --access public
--- src/stories/BgFade.stories.js
+++ src/stories/BgFade.stories.js
@@ -1,13 +1,39 @@
+import React from 'react';
+
+import { BgFade } from '../components/BgFade'
+import { bgImage } from './assets/bg.jpg'
+
+export default {
+  title: 'DesignStuff/BgFade',
+  component: BgFade
+}
+
+
+export const BgFadeBasic = () => <BgFade><div style={{minHeight: '1000px'}} /></BgFade>;
+export const BgFadeWithBgImage = () => <BgFade backgroundImage={bgImage}><div style={{minHeight: '1000px'}} /></BgFade>;
+
+/*
+
+const Template = (args) => <Page {...args} />
+
+export const Default = Template.bind({})
+LoggedIn.args = {}
+
+export const CustomBackground = Template.bind({})
+LoggedOut.args = { backgroundImage: bgImage }
+*/
+/*
 import React from 'react'
 import { storiesOf } from '@storybook/react'
 
 import { BgFade } from '../components/BgFade'
+import { bgImage } from './assets/bg.jpg'
 
 const stories = storiesOf('App BgFade', module)
 
 stories.add('BgFade', () => {
   return (
-    <BgFade>
+    <BgFade backgroundImage={bgImage}>
       <div style={{height: '100%'}}>
         <br /><br /><br /><br /><br /><br />
         <br /><br /><br /><br /><br /><br />
@@ -20,3 +46,4 @@ stories.add('BgFade', () => {
     </BgFade>
   )
 })
+*/
